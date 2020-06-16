c7f362a8ddc4d22078a1b8582094052959c5d8e3
zato91 <zatokey@gmail.com>
2020-06-16 17:56:15 -0400

    starting userEvent method

--- lib/app/models/UserEvent.rb
+++ lib/app/models/UserEvent.rb
@@ -3,4 +3,10 @@ class UserEvent < ActiveRecord::Base
     belongs_to :event
     has_many :venues, through: :events
     has_many :artists, through: :events
+
+    def self.new_user_event(self_user,user_event)
+        UserEvent.create(self_user, user_event)
+
+    end 
+
 end
