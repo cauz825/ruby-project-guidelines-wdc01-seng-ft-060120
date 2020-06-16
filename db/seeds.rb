9697091ce47689ba5768d73885fdcfcc2d34d6ba
zato91 <zatokey@gmail.com>
2020-06-16 12:26:16 -0400

    added event

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+Artist.destroy_all
+User.destroy_all
+Venue.destroy_all
+
+
 mike = User.create(username: "Mike")
 zara = User.create(username: "Zara")
 paul = User.create(username: "Paul")
@@ -7,6 +12,7 @@ foofighter = Artist.create(name:"foofighter", genre:"rock")
 drake = Artist.create(name:"drake", genre:"hip hop")
 
 bellagio = Venue.create(name:"bellagio", city:"las vegas")
-nyc = Venue.create(name:"404NYC", city:"create york")
+nyc = Venue.create(name:"404NYC", city:"new york")
 red_rocks = Venue.create(name:"red rocks", city:"denver")
 
+event1 = Event.create
--- lib/app/models/Event.rb
+++ lib/app/models/Event.rb
@@ -1,4 +1,4 @@
-class Events < ActiveRecord::Base
+class Event < ActiveRecord::Base
     belongs_to :artist
     belongs_to :venue
     has_many :users, through: :user_events
